30673b34b5805ffaa57b78b530f5dc7b827189b9
Pavel Saikov <pavsaikov@gmail.com>
2020-02-27 13:49:01 +0300

    feat: implement function 'reverse'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let stringNumber = new String(n);
+    let reverseStringNumber = '';
+
+    for (let i = stringNumber.length - 1; i >= 0; i--) {
+        reverseStringNumber += stringNumber[i];
+    }
+
+    let reverseNumber = parseInt(reverseStringNumber);
+
+    return reverseNumber;
 }
